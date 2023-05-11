adf9d5925a2c5e72490285d96959190991e221e2
Raphaël Carabeuf <raph.carabeuf@gmail.com>
2023-05-11 11:42:38 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.six_qui_prend {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires validatorfx;
+                requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+        
+    opens com.example.six_qui_prend to javafx.fxml;
+    exports com.example.six_qui_prend;
+}
